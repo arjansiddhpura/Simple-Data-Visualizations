4ca9e879d81ba455b6fa638939bc4870492aa810
Arjan Siddhpura <75858676+arjansiddhpura@users.noreply.github.com>
2021-07-13 10:32:40 +0200

    Add files via upload

---
+++ rw_visual.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+
+rw = RandomWalk(5000)
+rw.fill_walk()
+
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(16, 9))
+point_numbers = range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,  edgecolors='none', s=5)
+
+ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+    
+plt.show()
